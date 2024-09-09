90bbf377820fb1536553695d6256a5df4b7432ab
Feven-shi <fevenshibeshi@gmail.com>
2024-09-09 10:22:11 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <TodoInput />
+      <TodoList />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TodoList() {
+  return (
+    <div>
+      To do List
+    </div>
+  )
+}
